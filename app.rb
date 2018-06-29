9fc3b49ad3766bd417925f9c6a40dcbacfd739a5
Kenkichi Hattori <service@ahaufugyaha.com>
2018-06-29 12:55:08 +0900

    Add atis scraping

--- app.rb
+++ app.rb
@@ -4,9 +4,7 @@ require 'open-uri'
 
 bot = Discordrb::Commands::CommandBot.new token: ENV['ACCESS_TOKEN'], client_id: ENV['CLIENT_ID'], prefix: '!'
 
-bot.command :atis do |event|
-    icao = "RJTT"
-
+def scrape_metar(icao)
     metarurl = "https://www.time-j.net/MetarApp/MetarTaf/#{icao}"
 
     charset = nil
@@ -18,7 +16,11 @@ bot.command :atis do |event|
     doc = Nokogiri::HTML.parse(html, nil, charset)
     metar = doc.xpath('/html/body/div/div/div/div/table/tr/td')[17].inner_text
     
-    atisurl = "https://www.franomo.mlit.go.jp/Login.action"
+    return metar
+end
+
+def scrape_atis
+    atisurl = "https://www.franomo.mlit.go.jp"
     charset = nil
     html = open(atisurl) do |f|
         charset = f.charset
@@ -26,8 +28,16 @@ bot.command :atis do |event|
     end
 
     doc = Nokogiri::HTML.parse(html, nil, charset)
-    atis = doc.xpath('/html/body/div/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/div/table/tbody/tr/td').inner_text
+    atis = doc.xpath('//table[@class="atisinfo"]/tr/td').inner_text
 
+    return atis
+end
+
+bot.command :atis do |event|
+    icao = "RJTT"
+
+    metar = scrape_metar(icao)
+    atis = scrape_atis
 
     event.send_message("#{metar}")
     event.send_message("#{atis}")
